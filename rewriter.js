6848f0ef9a03b5b44eca1a552317057b3a91a96d
Andy Russell <arussell123@gmail.com>
2021-09-24 00:15:20 -0400

    use correct name for `includes` function

--- rewriter.js
+++ rewriter.js
@@ -1,5 +1,5 @@
 for (const link of document.getElementsByTagName('a')) {
-    if (link.href.contains('wowhead')) {
+    if (link.href.includes('wowhead')) {
         const url = new URL(link.href);
         url.hostname = url.hostname.replace(/wowhead\.com$/, 'wotlkdb.com')
         link.href = url.toString();
